5684989ecfd3952017bc00aae40a3b0c7f34657c
ocmplayofficial-coder <ocmplayofficial@gmail.com>
2026-06-19 22:05:56 +0530

    Update server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ async function startServer() {
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:8080",
-    "https://ocmplay.netlify.app",
+    "https://ocmplayy.netlify.app",
     "http://127.0.0.1:8080"
   ];
   

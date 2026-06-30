c36ca6e84fd721c772c456f119a1f44fc8584857
ocmplayofficial-coder <ocmplayofficial@gmail.com>
2026-06-30 10:26:45 +0530

    Modify allowedOrigins for CORS configuration
    Updated the allowedOrigins array to include additional localhost ports.

--- server.js
+++ server.js
@@ -56,12 +56,23 @@ async function startServer() {
 //   "https://ocmplay.netlify.app",
 //   "https://ocmplayy.netlify.app"
 // ];
+//   const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://127.0.0.1:5173",
+//   "http://127.0.0.1:5174",
+//   "http://localhost:8080",
+//   "http://127.0.0.1:8080",
+//   "https://ocmplay.netlify.app",
+//   "https://ocmplayy.netlify.app"
+// ];
   const allowedOrigins = [
   "http://localhost:5173",
+  "http://localhost:5174",
   "http://127.0.0.1:5173",
   "http://127.0.0.1:5174",
   "http://localhost:8080",
   "http://127.0.0.1:8080",
+
   "https://ocmplay.netlify.app",
   "https://ocmplayy.netlify.app"
 ];

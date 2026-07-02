d7e5f1a01d5bb27c67d4837049c3aa221658fa9f
ocmplayofficial-coder <ocmplayofficial@gmail.com>
2026-07-03 03:08:06 +0530

    Exclude matchmaking games from player count

--- services/teenpatti.service.js
+++ services/teenpatti.service.js
@@ -21,7 +21,7 @@ function broadcastTPQueueUpdate(queueKey) {
     const [feeStr, variantStr] = queueKey.split(':');
     const fee = Number(feeStr);
     for (const game of db.teenPattiGames.values()) {
-      if (game.entryFee === fee && game.variant === variantStr) {
+      if (game.entryFee === fee && game.variant === variantStr && game.status !== 'MATCHMAKING') {
         if (game.players) {
           if (game.players.A) count++;
           if (game.players.B) count++;

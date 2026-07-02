16f937f94180befe66e0237d507fbbc62cd29c7d
ocmplayofficial-coder <ocmplayofficial@gmail.com>
2026-07-03 04:29:45 +0530

    Update CORS allowed origins in server.js
    Added a new allowed origin for CORS configuration.

--- server.js
+++ server.js
@@ -65,6 +65,17 @@ async function startServer() {
 //   "https://ocmplay.netlify.app",
 //   "https://ocmplayy.netlify.app"
 // ];
+//   const allowedOrigins = [
+//   "http://localhost:5173",
+//   "http://localhost:5174",
+//   "http://127.0.0.1:5173",
+//   "http://127.0.0.1:5174",
+//   "http://localhost:8080",
+//   "http://127.0.0.1:8080",
+
+//   "https://ocmplay.netlify.app",
+//   "https://ocmplayy.netlify.app"
+// ];
   const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
@@ -74,7 +85,9 @@ async function startServer() {
   "http://127.0.0.1:8080",
 
   "https://ocmplay.netlify.app",
-  "https://ocmplayy.netlify.app"
+  "https://ocmplayy.netlify.app",
+
+  "https://6a46ecd639fed63ffafcb04d--ocmplayy.netlify.app"
 ];
 
   app.use(cors({

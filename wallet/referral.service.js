08ef355f6974966d5ed18414106c276fe30f7ae1
ocmplayofficial-coder <ocmplayofficial@gmail.com>
2026-07-05 03:36:56 +0530

    Modify referral reward system and bonus amount
    Update referral reward logic to include referrer ID and reduce bonus amount.

--- wallet/referral.service.js
+++ wallet/referral.service.js
@@ -6,12 +6,21 @@ export async function claimReferralBonus(user, code) {
     return null;
   }
 
+  const referrer = await UserModel.findOne({ referralCode: code.toUpperCase() });
+  if (!referrer) {
+    return null;
+  }
+  
+  if (user.referredBy || user._id.toString() === referrer._id.toString()) {
+    return null;
+  }
+
   // Atomic update to ensure reward is given ONLY ONCE
   const updatedUser = await UserModel.findOneAndUpdate(
     { _id: user._id, referralRewardGiven: { $ne: true } },
     { 
-      $inc: { depositBalance: 50, walletBalance: 50 },
-      $set: { referralRewardGiven: true, rewardProcessedAt: new Date() }
+      $inc: { depositBalance: 10, walletBalance: 10 },
+      $set: { referralRewardGiven: true, rewardProcessedAt: new Date(), referredBy: referrer._id }
     },
     { new: true }
   );
@@ -21,17 +30,24 @@ export async function claimReferralBonus(user, code) {
     return null;
   }
 
+  // Increment referrer's count
+  await UserModel.updateOne(
+     { _id: referrer._id },
+     { $inc: { referralCount: 1 } }
+  );
+
   // Sync memory object just in case the caller needs it
   user.depositBalance = updatedUser.depositBalance;
   user.walletBalance = updatedUser.walletBalance;
   user.referralRewardGiven = updatedUser.referralRewardGiven;
   user.rewardProcessedAt = updatedUser.rewardProcessedAt;
+  user.referredBy = updatedUser.referredBy;
 
   const tx = addTransaction({
     type: "BONUS",
-    amount: 50.00,
+    amount: 10.00,
     status: "SUCCESS",
-    method: `Referral Invite Bonus (${code})`
+    method: `Referral Invite Bonus`
   }, user);
 
   return tx;

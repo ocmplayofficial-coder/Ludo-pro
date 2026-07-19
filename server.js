2cb29b5e5d71277456a7a05b46ea257fade1e37d
ocmplayofficial-coder <ocmplayofficial@gmail.com>
2026-07-20 04:31:37 +0530

    Reorder allowedOrigins in server.js

--- server.js
+++ server.js
@@ -29,17 +29,17 @@ async function startServer() {
   const PORT = process.env.PORT || 5000;
 
   // Middlewares
-  const allowedOrigins = [
-    "http://localhost:3000",
-    "http://localhost:5173",
-    "http://localhost:5174",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
-    "http://localhost:8080",
-    "https://ocmplay.netlify.app",
-    "https://ocmplayy.netlify.app",
-    "http://127.0.0.1:8080"
-  ];
+ const allowedOrigins = [
+  "http://localhost:3000",
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "http://127.0.0.1:5173",
+  "http://127.0.0.1:5174",
+  "http://localhost:8080",
+  "http://127.0.0.1:8080",
+  "https://ocmplay.netlify.app",
+  "https://ocmplayy.netlify.app"
+];
   
 
   app.use(cors({
